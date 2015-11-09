7ca88557358efd90664ebcf4b50b7b0bf3659840
Fahad Chilli <purplechilli_411@yahoo.com>
2015-11-09 11:40:43 +0500

    abc

--- hello/src/hello/hel.java
+++ hello/src/hello/hel.java
@@ -3,6 +3,7 @@ package hello;
 public class hel {
 public static void main(String args[]){
 	
-	System.out.print("lets go rock....................");
+	System.out.println("lets go rock....................");
+	System.out.println("party  all night....................");
 
 }}
