981d9be0c1aa69163c1d248dd17f4d79d99ffc69
Fahad Chilli <purplechilli_411@yahoo.com>
2015-11-09 11:12:38 +0500

    hell

--- fah/src/fah/demo.java
+++ fah/src/fah/demo.java
@@ -3,4 +3,11 @@ package fah;
 public class demo {
 	public static void main(String args[]){
 System.out.println("lets go rock/.............");
+System.out.println("hav fun/.............");
+
+System.out.println("party alnight/.............");
+System.out.println("okkkkk/.............");
+
+
 }}
+                                                                 
